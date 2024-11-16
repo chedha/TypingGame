05038a96da3944814da49199547ad877020c270a
chedha <chedha@gmail.com>
2024-11-16 14:07:10 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf, NgFor } from '@angular/common';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  userInput = '';
+  success = false;
+
+  onChange(event: Event) {
+    this.userInput = (event.target as HTMLInputElement).value;
+    if (this.randomText === this.userInput) {
+      this.success = true;
+    }
+    console.log((event.target as HTMLInputElement).value);
+  }
+
+  compare (randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
